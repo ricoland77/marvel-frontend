bb4ace73335c2b4be50803b8cb1f9410b332e45b
mac-ricoland <ricoland@MacBook-Pro.local>
2023-01-06 10:37:11 +0100

    change url

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -22,7 +22,7 @@ const Characters = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `https://backend-marvel-ricoland.netlify.app/characters?limit=${limit}&skip=${skip}&name=${name}`
+          `https://frontend-marvel-ricoland.netlify.app/characters?limit=${limit}&skip=${skip}&name=${name}`
         );
 
         setData(response.data);
